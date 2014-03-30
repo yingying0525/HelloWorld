9c37eaf8318fca8db00e215e0e9bf88556811915
yingying0525 <279443374@qq.com>
2014-03-30 15:34:58 +0800

    new hello

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.example.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello world");
+	    System.out.println("hello world!!");
     }
 }
